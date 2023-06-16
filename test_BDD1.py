2ef49c69ef0be98e29ddee86b0dd09c05fd23b62
Asonoda <asonoda91@gmail.com>
2023-06-16 04:06:27 -0300

    BDD1

---
+++ test_BDD1.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Chrome()
+
+browser.maximize_window()
+browser.get("https://advantageonlineshopping.com/")
+
+lupa = browser.find.element(By.ID, "menuSearch")
+print(lupa.is_displayed())
+assert lupa.is_displayed()

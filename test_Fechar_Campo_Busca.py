158f4ee6223b904558c3d85aa972334f5b01a7f9
Asonoda <asonoda91@gmail.com>
2023-06-20 01:34:26 -0300

    FecharCampoBusca

--- test_Fechar_Campo_Busca.py
+++ test_Fechar_Campo_Busca.py
@@ -5,21 +5,18 @@ from selenium.webdriver.common.by import By
 browser = webdriver.Chrome()
 browser.maximize_window()
 browser.get("https://advantageonlineshopping.com/")
+time.sleep(10)
 
 #find elemente()
 lupa = browser.find_element(By.ID, "menuSearch")
 time.sleep(10)
 
-#clickc()
+#click()
 lupa.click()
 time.sleep(10)
 assert lupa.is_displayed()
 
 #find elemente()
-lupa = browser.find_element(By.ID, "menuSearch")
-time.sleep(10)
-
-#clickc()
-lupa.click()
-time.sleep(10)
-assert lupa.is_displayed()
+close = browser.find_element(By.XPATH, "(https://advantageonlineshopping.com/css/images/closeDark.png")
+close.click()
+time.sleep(5)

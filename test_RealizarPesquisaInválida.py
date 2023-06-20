09f65c53567571b0a4ba1b102fd0d2ba77f867b1
Asonoda <asonoda91@gmail.com>
2023-06-20 06:54:32 -0300

    RealizarPesquisaInválida

--- test_RealizarPesquisaInválida.py
+++ test_RealizarPesquisaInválida.py
@@ -1,5 +1,4 @@
 import time
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -14,11 +13,10 @@ time.sleep(10)
 
 # click()
 lupa.click()
-time.sleep(10)
-assert lupa.is_displayed()
+time.sleep(5)
 
 # find element()
-browser.find_element(By.NAME, "mobile_search").send_keys("Teste")
+browser.find_element(By.XPATH, '//*[@id="mobileSearch"]/input').send_keys('Teste')
 time.sleep(5)
 
 # click()

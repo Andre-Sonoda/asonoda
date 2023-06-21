19e9c356bacd89473b658c579b016f31f3e83220
Asonoda <asonoda91@gmail.com>
2023-06-21 13:53:00 -0300

    RealizarPesquisaInválida

--- test_RealizarPesquisaInválida.py
+++ test_RealizarPesquisaInválida.py
@@ -1,21 +1,18 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.by import browser
 
-def test_Teste1():
-    browser: webdriver = webdriver.Chrome()
-    browser.implicitly_wait(20)
+browser: webdriver = webdriver.Chrome()
+browser.implicitly_wait(20)
 
-    browser.maximize_window()
-    browser.get("https://advantageonlineshopping.com/")
+browser.maximize_window()
+browser.get("https://advantageonlineshopping.com/")
 
 # find element()
-def test_Teste2():
-    lupa = browser.find_element(By.ID, "menuSearch")
-    campo_busca = browser.find_element(By.NAME, "mobile_search")
+lupa = browser.find_element(By.ID, "menuSearch")
+campo_busca = browser.find_element(By.NAME, "mobile_search")
 
 #click()
-    lupa.click()
+lupa.click()
 
 #send_keys()
-    campo_busca.send_keys("Teste")
+campo_busca.send_keys("Teste")

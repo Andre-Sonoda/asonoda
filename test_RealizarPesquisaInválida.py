a4619ab205d6e045a7849cd8f50dc3f84d6c6a8a
Asonoda <asonoda91@gmail.com>
2023-06-22 06:27:03 -0300

    RealizarPesquisaInválida

--- test_RealizarPesquisaInválida.py
+++ test_RealizarPesquisaInválida.py
@@ -1,9 +1,7 @@
 import time
-from telnetlib import EC
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import wait
 
 browser: webdriver = webdriver.Chrome()
 browser.implicitly_wait(20)
@@ -11,20 +9,25 @@ browser.implicitly_wait(20)
 browser.maximize_window()
 browser.get("https://advantageonlineshopping.com/")
 
+
 # find element()
 def test_Teste1():
     lupa = browser.find_element(By.ID, "menuSearch")
-# click()
+    # click()
     lupa.click()
 
-#def test_Teste2():
-    #    busca = browser.find_element(By.NAME, "mobile_search")
-# send_keys()
-#    busca.send_keys("Teste")
 
 def test_Teste2():
-    busca = browser.find_element(By.NAME, "mobile_search")
-    wait.until(EC.element_to_be_selected(By.id, "mobileSearch"))
-    time.sleep(2)
-# send_keys()
+    busca = browser.find_element(By.ID, "autoComplete")
+    # send_keys()
     busca.send_keys("Teste")
+
+
+def test_Teste3():
+    lupa = browser.find_element(By.ID, "menuSearch")
+    # click()
+    lupa.click()
+
+    text_esperado = 'No results for "Teste"'
+    text_obtido = 'No results for "Teste"'
+    assert text_esperado in text_obtido, f"Esperado {text_esperado}. Atual {text_obtido}."

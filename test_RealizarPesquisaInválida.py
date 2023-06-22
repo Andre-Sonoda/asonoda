4dc202f0a2a9a2817c4ed1640e9e51a8eca8b2c4
Asonoda <asonoda91@gmail.com>
2023-06-21 23:23:41 -0300

    RealizarPesquisaInválida

--- test_RealizarPesquisaInválida.py
+++ test_RealizarPesquisaInválida.py
@@ -1,5 +1,9 @@
+import time
+from telnetlib import EC
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import wait
 
 browser: webdriver = webdriver.Chrome()
 browser.implicitly_wait(20)
@@ -13,7 +17,14 @@ def test_Teste1():
 # click()
     lupa.click()
 
+#def test_Teste2():
+    #    busca = browser.find_element(By.NAME, "mobile_search")
+# send_keys()
+#    busca.send_keys("Teste")
+
 def test_Teste2():
     busca = browser.find_element(By.NAME, "mobile_search")
+    wait.until(EC.element_to_be_selected(By.id, "mobileSearch"))
+    time.sleep(2)
 # send_keys()
     busca.send_keys("Teste")

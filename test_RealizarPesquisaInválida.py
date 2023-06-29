bcbd030acdcb6202dc8dcea1132ff0c502fdd1a4
Asonoda <asonoda91@gmail.com>
2023-06-29 07:41:31 -0300

    test_RealizarPesquisaInválida

--- test_RealizarPesquisaInválida.py
+++ test_RealizarPesquisaInválida.py
@@ -1,31 +1,31 @@
-from selenium import webdriver
+import time
+import pytest
 from selenium.webdriver.common.by import By
-
-browser: webdriver = webdriver.Chrome()
-browser.implicitly_wait(20)
-
-browser.maximize_window()
-browser.get("https://advantageonlineshopping.com/")
+import conftest
 
 
-# find element()
-def test_Teste1():
-    lupa = browser.find_element(By.ID, "menuSearch")
-    # click()
-    lupa.click()
+@pytest.mark.usefixtures("setup_teardown")
+class TestBusca:
+        def test_Teste1(self):
+            browser = conftest.browser
+            lupa = browser.find_element(By.ID, "menuSearch")
+            # click()
+            lupa.click()
 
 
-def test_Teste2():1
-    busca = browser.find_element(By.ID, "autoComplete")
-    # send_keys()
-    busca.send_keys("Teste")
+        def test_Teste2(self):
+            browser = conftest.browser
+            busca = browser.find_element(By.ID, "autoComplete")
+            # send_keys()
+            busca.send_keys("Teste")
 
 
-def test_Teste3():
-    lupa = browser.find_element(By.ID, "menuSearch")
-    # click()
-    lupa.click()
+        def test_Teste3(self):
+            browser = conftest.browser
+            lupa = browser.find_element(By.ID, "menuSearch")
+            # click()
+            lupa.click()
 
-    text_esperado = 'No results for "Teste"'
-    text_obtido = 'No results for "Teste"'
-    assert text_esperado in text_obtido, f"Esperado {text_esperado}. Atual {text_obtido}."
+            text_esperado = 'No results for "Teste"'
+            text_obtido = 'No results for "Teste"'
+            assert text_esperado in text_obtido, f"Esperado {text_esperado}. Atual {text_obtido}."

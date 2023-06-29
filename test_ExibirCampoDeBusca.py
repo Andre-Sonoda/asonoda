86d6882b25efca0cf57706660c00a7f8dfa77ece
Asonoda <asonoda91@gmail.com>
2023-06-29 07:40:40 -0300

    test_ExibirCampoDeBusca

--- test_ExibirCampoDeBusca.py
+++ test_ExibirCampoDeBusca.py
@@ -1,21 +1,16 @@
 import time
-
-from selenium import webdriver
+import pytest
 from selenium.webdriver.common.by import By
+import conftest
 
-browser = webdriver.Chrome()
+@pytest.mark.usefixtures("setup_teardown")
+class TestBusca:
+    def test_ExibirCampoBusca(self):
+        browser = conftest.browser
+        #find elemente()
+        lupa = browser.find_element(By.ID, "menuSearch")
 
-browser.maximize_window()
-browser.get("https://advantageonlineshopping.com/")
-
-#find elemente()
-lupa = browser.find_element(By.ID, "menuSearch")
-time.sleep(10)
-
-#clickc()
-lupa.click()
-time.sleep(10)
-
-#print(lupa.is_displayed())
-
-assert lupa.is_displayed()
+        #clickc()
+        lupa.click()
+        assert lupa.is_displayed()
+        time.sleep(5)

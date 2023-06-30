133c297d1b1a778935636de22d323403194c2902
Asonoda <asonoda91@gmail.com>
2023-06-30 17:05:14 -0300

    adicionei mais um comnetário

--- features/scenario.feature
+++ features/scenario.feature
@@ -1,11 +1,8 @@
-Feature: Scenario Outline
+Feature: Teste Site Advantage
 
-  Scenario: Teste com produtos
-    Given
-    When
-    And
-    Then
-
-
-
-
+  Scenario: Test
+    Given I ON THE <SITE>
+    When  I click on the top menu magnifying glass
+    And   I type <PRODUTO>
+    And   press search/enter
+    Then  the product page is displayed
--- pages/lupa.py
+++ pages/lupa.py
@@ -17,6 +17,7 @@ class LupaPage:
         lupa = wait.until(EC.element_to_be_clickable((By.ID, "menuSearch")))
         #lupa = conftest.browser.find_element(By.ID, "menuSearch")
         #teste
+        # teste2
         lupa.click()
         close = conftest.browser.find_element(By.CSS_SELECTOR, "#search > div > div > img")
         assert close.is_enabled()

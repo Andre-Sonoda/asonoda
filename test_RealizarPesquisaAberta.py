e846b58576c201408340efab57dcb70bbecd8e17
Asonoda <asonoda91@gmail.com>
2023-07-04 11:44:08 -0300

    RealizarPesquisaAberta

--- test_RealizarPesquisaAberta.py
+++ test_RealizarPesquisaAberta.py
@@ -1,22 +1,22 @@
 import time
+from threading import Event
 import pytest
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import conftest
+from pages.lupa import LupaPage
 
 
 @pytest.mark.usefixtures("setup_teardown")
 class TestBusca:
-        def test_Teste1(self):
-            browser = conftest.browser
-            lupa = browser.find_element(By.ID, "menuSearch")
-            #click()
-            lupa.click()
-            assert lupa.is_displayed()
-
-
-        def test_Teste2(self):
-            browser = conftest.browser
-            lupa = browser.find_element(By.ID, "menuSearch")
-            #click()
-            lupa.click()
-            assert lupa.is_displayed()
+    def test_RealizarPesquisaAberta(self):
+        browser = conftest.browser
+        novaLupa = LupaPage()
+        novaLupa.acessar_lupa()
+        delay = 5
+        Event().wait(delay)
+        novaLupa = LupaPage()
+        novaLupa.acessar_lupa()
+        Texto = conftest.browser.find_element(By.ID, "searchResultLabel")
+        assert Texto.is_enabled()

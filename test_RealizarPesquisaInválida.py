a10c0c4a9b22df3e5a7e8403501695218e4d88a5
Asonoda <asonoda91@gmail.com>
2023-07-04 13:06:52 -0300

    RealizarPesquisaInválida

--- test_RealizarPesquisaInválida.py
+++ test_RealizarPesquisaInválida.py
@@ -1,31 +1,27 @@
-import time
 import pytest
-from selenium.webdriver.common.by import By
 import conftest
+from selenium.webdriver.common.by import By
+from threading import Event
+from pages.lupa import LupaPage
 
 
 @pytest.mark.usefixtures("setup_teardown")
 class TestBusca:
+
         def test_Teste1(self):
             browser = conftest.browser
-            lupa = browser.find_element(By.ID, "menuSearch")
-            # click()
-            lupa.click()
-
-
-        def test_Teste2(self):
-            browser = conftest.browser
-            busca = browser.find_element(By.ID, "autoComplete")
-            # send_keys()
+            novaLupa = LupaPage()
+            novaLupa.acessar_lupa()
+            delay = 5
+            Event().wait(delay)
+            busca = conftest.browser.find_element(By.ID, "autoComplete")
             busca.send_keys("Teste")
-
-
-        def test_Teste3(self):
-            browser = conftest.browser
-            lupa = browser.find_element(By.ID, "menuSearch")
-            # click()
-            lupa.click()
-
+            delay = 5
+            Event().wait(delay)
+            novaLupa = LupaPage()
+            novaLupa.acessar_lupa()
             text_esperado = 'No results for "Teste"'
             text_obtido = 'No results for "Teste"'
+            delay = 5
+            Event().wait(delay)
             assert text_esperado in text_obtido, f"Esperado {text_esperado}. Atual {text_obtido}."

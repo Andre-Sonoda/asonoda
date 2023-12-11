e2952dbd0e8296865cc21abd9d86e6413fffba42
Asonoda <asonoda91@gmail.com>
2023-12-11 14:54:15 -0300

    test_TeclaEnter

--- test_TeclaEnter.py
+++ test_TeclaEnter.py
@@ -3,8 +3,8 @@ import pytest
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 import conftest
+from pages.base_page import BasePage
 from pages.lupa import LupaPage
-
 
 @pytest.mark.usefixtures("setup_teardown")
 class TestBusca:
@@ -17,17 +17,20 @@ class TestBusca:
             Event().wait(delay)
             busca = conftest.browser.find_element(By.ID, "autoComplete")
             busca.send_keys("Teste")
-
-            def pressionar_tecla(self, key):
-                buscar = self.find_element("menuSearch")
-                if key == "ENTER":
-                    buscar.send_keys(Keys.ENTER)
-                    assert buscar.is_enabled()
-
             delay = 5
             Event().wait(delay)
-            text_esperado = 'No results for "Teste"'
-            text_obtido = 'No results for "Teste"'
-            delay = 5
-            Event().wait(delay)
-            assert text_esperado in text_obtido, f"Esperado {text_esperado}. Atual {text_obtido}."
+
+        def pressionar_tecla(self, locator, key):
+            enter_busca = BasePage()
+            enter_busca.encontrar_elemento(locator)
+            enter_busca = self.encontrar_elemento("menuSearch")
+            if key == "ENTER":
+               enter_busca.send_keys(Keys.ENTER)
+
+        delay = 5
+        Event().wait(delay)
+        text_esperado = 'No results for "Teste"'
+        text_obtido = 'No results for "Teste"'
+        delay = 5
+        Event().wait(delay)
+        assert text_esperado in text_obtido, f"Esperado {text_esperado}. Atual {text_obtido}."
